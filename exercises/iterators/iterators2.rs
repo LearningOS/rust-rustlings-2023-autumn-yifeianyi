c2a1f6efa04aa19ea7ffc3678fce8d37c291ce31
YiFeiAnYi-lzy <910002662@qq.com>
2023-10-13 10:35:51 +0800

    2023/10/13 update_end

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -17,8 +16,8 @@ pub fn capitalize_first(input: &str) -> String {
         None => String::new(),
         Some(first) => {
             let mut ret = input.to_string();
-            
-        }
+            ret
+        },
 
     }
 }
@@ -28,7 +27,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // vec![];
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -36,7 +36,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // String::new()
+    words.iter().fold(String::new(), |mut acc, word|{
+        acc.push_str(&capitalize_first(word));
+        acc
+    })
 }
 
 #[cfg(test)]
