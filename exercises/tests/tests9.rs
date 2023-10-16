84d30273a967d6bdb04e7787dae3debd2b15fef4
YiFeiAnYi-lzy <910002662@qq.com>
2023-10-16 08:25:53 +0800

    2023/10/16 update rustlings finished.

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,24 +27,20 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 // use crate::foo::my_demo_function;
-#[link_name = "test9.rs"]
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
-    #[no_mangle]
+    #[link_name="my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
         a
     }
-    // fn my_demo_function_alias(a: u32) -> u32{
-    //     a
-    // }
 }
 
 #[cfg(test)]
@@ -59,9 +55,9 @@ mod tests {
         //
         // SAFETY: We know those functions are aliases of a safe
         // Rust function.
-        // unsafe {
+        unsafe {
             my_demo_function(123);
             my_demo_function_alias(456);
-        // }
+        }
     }
 }
